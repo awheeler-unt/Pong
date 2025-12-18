5af40bc0de1e6d82ca600c3bb1862f52970006af
awheeler-unt <alexiswheeler@my.unt.edu>
2025-12-18 15:41:14 -0800

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,7 +55,7 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("fireplace sound.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = 15; //turn up volume
+ambientSound.player.volume.value = 25; //turn up volume
 
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
